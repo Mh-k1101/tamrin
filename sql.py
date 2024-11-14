824aa4849ba5ae67ee1bbf63944d352e2bca90fe
Mh-k1101 <karempmohammad@gmail.com>
2024-11-14 15:58:11 +0330

    Add files via upload

---
+++ sql.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+class Database:
+    def __init__(self, db):
+  
+        self.con = sqlite3.connect(db)
+        self.cur = self.con.cursor()
+    
+        self.cur.execute('''
+            CREATE TABLE IF NOT EXISTS login(id INTEGER PRIMARY KEY,
+                fname TEXT,
+                lname TEXT,
+                email TEXT,
+                password TEXT)''')
+        self.con.commit()
+
+    def insert(self, fname, lname, email, password):
+
+        self.cur.execute('INSERT INTO login (fname, lname, email, password) VALUES (?, ?, ?, ?)', 
+                         (fname, lname, email, password))
+        self.con.commit()
+
+    def fetch(self, email, password):
+  
+        self.cur.execute('SELECT * FROM login WHERE email = ? AND password = ?', (email, password))
+        return self.cur.fetchall()
+
+    def close(self):
+   
+        self.con.close()
